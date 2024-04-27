d7aacf08ac92012fb151851e2049ef4b88422eb9
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-04-27 12:39:37 +0530

    Understands Props in React

--- 07 Props/src/App.jsx
+++ 07 Props/src/App.jsx
@@ -1,34 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const ParentComponent = () => {
+     return <ChildComponent name = "Kshitiz" age = {18} />
+  }
+
+  const ChildComponent = (bhaiKaData) => {                      // So, the convention is to use "props", still can use anything like bhaiKaData
+    return (
+      <div>{bhaiKaData.name} is {bhaiKaData.age} years old</div>
+    )
+  }
+
+  return(
+    <div>
+      <ParentComponent/>
+    </div>
   )
 }
 
